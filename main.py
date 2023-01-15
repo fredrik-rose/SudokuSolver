77773578bd32cd75b04b6f29614c148eea6e48f4
Fredrik Rose <fredrik.rose.developer@gmail.com>
2023-01-15 14:41:19 +0100

    Add support for input files
    The application now support a user provided input file
    containing a sudoku puzzle. This makes the applicaiton
    much more useful.
    
    The example.py is removed as it is replaced by the input
    file.

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 """
 Program entry point.
 """
+import argparse
+import pathlib
 import time
 
-import example as ex
+import numpy as np
+
 import sudokusolver.sudokusolver as solver
+
+
+EMPTY_CELL_VALUE = 0
+
+
+def parse_sudoku(file_path, empty_cell_value):
+    """
+    Parse a sudoku puzzle fro ma file.
+    :param file_path: Path to file with the sudoku puzzle.
+    :param empty_cell_value: Value of an empty cell.
+    """
+    sudoku = []
+    with open(file_path) as file:
+        for line in file:
+            line = line.rstrip().split(" ")
+            sudoku.append([int(e) if e != "-" else 0 for e in line])
+    return np.array(sudoku)
+
+
+def print_sudoku(sudoku):
+    """
+    Print a sudoku puzzle.
+    :param sudoku: Sudoku puzzle.
+    """
+    for line in sudoku:
+        print(" ".join(str(e) for e in line))
 
 
 def solve(sudoku, empty_cell_value):
     """
-    Solves a sudoku puzzle.
+    Solve a sudoku puzzle.
     :param sudoku: Sudoku puzzle.
     :param empty_cell_value: Value of an empty cell.
     """
-    print("{}\n".format(sudoku))
+    print("Input:")
+    print_sudoku(sudoku)
+    print("")
     start = time.time()
     success = solver.solve(sudoku, empty_cell_value)
     end = time.time()
@@ -21,19 +52,21 @@ def solve(sudoku, empty_cell_value):
         print("Successfully solved!")
     else:
         print("Could not solve!")
-    print("Took {} seconds.\n".format(end - start))
-    print("{}".format(sudoku))
+    print("Took {:.2f} seconds.\n".format(end - start))
+    print("Solution:")
+    print_sudoku(sudoku)
 
 
 def main():
     """
     Program entry point.
     """
-    empty_cell_value = 0
-    print("Easy sudoku puzzle:")
-    solve(ex.easy(empty_cell_value), empty_cell_value)
-    print("\nHard sudoku puzzle (this will take some time to solve):")
-    solve(ex.hard(empty_cell_value), empty_cell_value)
+    parser = argparse.ArgumentParser(description="A Sudoku puzzle solver.")
+    parser.add_argument("input", type=pathlib.Path,
+                        help="File with the puzzle to solve. Use - for empty cells and space as separator.")
+    args = parser.parse_args()
+    sudoku = parse_sudoku(args.input, EMPTY_CELL_VALUE)
+    solve(sudoku, EMPTY_CELL_VALUE)
 
 
 if __name__ == "__main__":
